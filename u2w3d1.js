df1ed13e0790236dab299a4d9223b9da291cc136
Kenny Boateng <kennyboateng.99@gmail.com>
2024-06-10 16:42:27 +0200

    esercizio 2

--- u2w3d1.js
+++ u2w3d1.js
@@ -30,3 +30,45 @@ const User3 = new User("Giacomo", "Poretti", 68, "Villa Cortese");
 console.log(User1.compareAge(User2));
 console.log(User2.compareAge(User1));
 console.log(User3.compareAge(User1));
+
+/* creare classe pet*/
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+  sameOwner(otherPet) {
+    return this.ownerName === otherPet.ownerName;
+  }
+}
+
+const form = document.querySelector("form");
+const petList = document.createElement("ul");
+document.body.appendChild(petList);
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+
+  const petName = document.getElementById("petName").value;
+  const ownerName = document.getElementById("ownerName").value;
+  const species = document.getElementById("species").value;
+  const breed = document.getElementById("breed").value;
+
+  const newPet = new Pet(petName, ownerName, species, breed);
+
+  const petItem = document.createElement("li");
+  petItem.textContent =
+    "Name: " +
+    newPet.petName +
+    ", Owner: " +
+    newPet.ownerName +
+    ", Species: " +
+    newPet.species +
+    ", Breed: " +
+    newPet.breed;
+  petList.appendChild(petItem);
+  form.reset();
+};
